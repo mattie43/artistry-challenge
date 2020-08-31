62891f285af6e45b97f1c00b568bc6701cd65bc8
Mattie43 <ericksenm93@ymail.com>
2020-08-31 12:08:34 -0400

    deliverables done, cleaning/organizing

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,5 @@
 class Artist < ApplicationRecord
+    has_and_belongs_to_many :instruments
+    validates :name, presence: true
+    validates :name, :title, uniqueness: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
+Artist.destroy_all
+Instrument.destroy_all
+ArtistsInstrument.destroy_all
+
 Artist.create(name: "Michael Jackson", age: rand(50), title: "King of Pop")
 Artist.create(name: "Stevie Wonder", age: rand(50), title: "Little Stevie Wonder")
-Artist.create(name: "Elvis Presley", age: rand(50), title: "the King")
+Artist.create(name: "Elvis Presley", age: rand(50), title: "The King")
 Artist.create(name: "Whitney Houston", age: rand(50), title: "The Voice")
 Artist.create(name: "Beyoncé", age: rand(50), title: "Destiny's Child")
 
@@ -8,3 +12,7 @@ Instrument.create(name: "Guitar", classification: "Strings")
 Instrument.create(name: "Violin", classification: "Strings")
 Instrument.create(name: "Flute", classification: "Woodwind")
 Instrument.create(name: "Xylophone", classification: "Percussion")
+
+20.times do
+    ArtistsInstrument.create(artist: Artist.all.sample, instrument: Instrument.all.sample)
+end
